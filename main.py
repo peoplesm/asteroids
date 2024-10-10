d093a896cf55dce2e2523cf38f99e71abf4ae1ee
peoplesm <mep.summit@gmail.com>
2024-10-09 18:35:53 -0600

    Limit framerate to 60 fps.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
 
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,9 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # Limit framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
