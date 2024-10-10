381a4eb3be80c916469efa4e574b0f1a7d7c6d6a
peoplesm <mep.summit@gmail.com>
2024-10-09 18:10:21 -0600

    Removed screen dimension print statements

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from constants import *
 
 
 def main():
-    pygame.init
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
