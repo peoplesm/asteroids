f1c212008c3356c5b1cfca5a8c8b315e70d31b6b
peoplesm <mep.summit@gmail.com>
2024-10-12 11:38:56 -0600

    Collision detection and game over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -32,6 +33,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for obj in asteroids:
+            if obj.collision(player) == True:
+                print("Game Over!")
+                sys.exit()
 
         screen.fill((0, 0, 0))
 
